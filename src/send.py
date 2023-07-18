2806d3b9c818fc5aed0d794cd9508c32ebe2fca5
Tanish <tkhadse2@illinois.edu>
2023-07-18 13:03:56 -0500

    code to open C++ exec

--- src/send.py
+++ src/send.py
@@ -10,8 +10,9 @@ dir = os.path.dirname(cwd)
 def send_adj_matrix(matrix):
     serialized_matrix = np.array2string(matrix, separator=",")
 
-    cpp_exec_file = './' + os.path.join(dir, os.path.join('graphs', os.path.join('bin', 'exec')))
-    process = subprocess.Popen([cpp_exec_file], stdin=subprocess.PIPE, stout=subprocess.PIPE, text=True)
+    # cpp_exec_file = '.' + os.path.join(dir, os.path.join('graphs', os.path.join('bin', 'exec')))
+    cpp_exec_file = "../graph/bin/exec"
+    process = subprocess.Popen([cpp_exec_file], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
 
     process.stdin.write(serialized_matrix + '\n')
     process.stdin.flush()
@@ -25,5 +26,5 @@ def send_adj_matrix(matrix):
     return result
 
 if __name__ == "__main__":
-    adjacencyMatrix = []
+    adjacencyMatrix = np.array([[0, 1], [0, 1], [1, 1]])
     result = send_adj_matrix(adjacencyMatrix)
