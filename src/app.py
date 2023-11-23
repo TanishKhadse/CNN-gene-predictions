798b0970b3dc40cd91f65bc1be68631908df1acf
Tanish <tkhadse2@illinois.edu>
2023-11-23 17:30:31 -0600

    neo4j integration

--- src/app.py
+++ src/app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, request
+from neo4j import GraphDatabase, basic_auth  
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+user = os.getenv("NEO4J_USER")
+password = os.getenv("NEO4J_PW")
+uri = os.getenv("NEO4J_URI")
 
 app = Flask(__name__)
+db = GraphDatabase.driver(uri, auth=(user, password))
 
 @app.route('/')
 def index():
